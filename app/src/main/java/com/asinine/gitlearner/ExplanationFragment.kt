2dff6dbee94712e8512206e98082378a5f0351e4
Abraham Mathew <abraham.mathew@seqato.com>
2022-07-28 12:10:54 +0530

    feature created

---
+++ app/src/main/java/com/asinine/gitlearner/ExplanationFragment.kt
@@ -0,0 +1,22 @@
+package com.asinine.gitlearner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class ExplanationFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explanation, container, false)
+    }
+}
